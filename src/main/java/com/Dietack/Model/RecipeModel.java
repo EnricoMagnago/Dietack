78b265d162a5d273fe254be4d65ef87de21369c8
Enrico Testori <hypertesto@gmail.com>
2016-03-10 18:48:09 +0100

    Abbozzo non testato metodo getRecipeById

---
+++ src/main/java/com/Dietack/Model/RecipeModel.java
@@ -0,0 +1,72 @@
+package com.Dietack.Model;
+
+import com.Dietack.Model.Bean.Ingredient;
+import com.Dietack.Model.Bean.Recipe;
+
+import javax.naming.NamingException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by hypertesto on 10/03/16.
+ */
+public class RecipeModel {
+
+    public static Recipe getRecipeById(int id) throws SQLException, NamingException {
+
+        Connection connection;
+        ResultSet rs, rs2;
+        Recipe res;
+
+        String query = "SELECT * from ingredienti\n" +
+                "WHERE id = ? ;";
+
+        connection = Connector.getConnection();
+        PreparedStatement ps = connection.prepareStatement(query);
+        ps.setInt(1, id);
+
+        rs = ps.executeQuery();
+
+        if ( rs.next() ) {
+
+            res = new Recipe();
+            res.setId(rs.getInt("id_ricetta"));
+            res.setName(rs.getString("nome"));
+            res.setFoto(rs.getString("foto"));
+
+            //now generate the list of pairs
+            String queryIngredienti = "SELECT * from ingredienti_ricetta\n" +
+                    "WHERE id_ricetta = ? ;";
+            PreparedStatement ps2 = connection.prepareStatement(queryIngredienti);
+            ps2.setInt(1, id);
+
+            rs2 = ps2.executeQuery();
+
+            while ( rs2.next() ) {
+
+                Ingredient temp = IngredientModel.getIngredientById(rs2.getInt("id_ingrediente"));
+                res.addIngredient(temp, rs.getDouble("quantita"));
+
+            }
+
+            rs2.close();
+            ps2.close();
+
+        } else {
+
+            res = null;
+
+        }
+
+        rs.close();
+        ps.close();
+        connection.close();
+
+        return res;
+
+
+    }
+
+}
