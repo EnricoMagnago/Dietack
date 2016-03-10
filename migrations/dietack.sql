d5457fa4440aa722627caef078c1eba7b746f696
Enrico Testori <hypertesto@gmail.com>
2016-03-10 20:47:17 +0100

    Aggiunto di nuovo il campo mancante

--- migrations/dietack.sql
+++ migrations/dietack.sql
@@ -29,6 +29,7 @@ CREATE TABLE `ingredienti` (
   `nome` varchar(50) NOT NULL,
   `kcal` float NOT NULL,
   `foto` varchar(200) DEFAULT NULL,
+  `um` varchar(5) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -154,4 +155,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-03-10 20:35:58
+-- Dump completed on 2016-03-10 20:46:06
