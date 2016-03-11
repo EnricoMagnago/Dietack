b62c26b967ce54535824d65e9a80d19c88b7fe33
EnricoMagnago <en.magnago@gmail.com>
2016-03-11 10:05:15 +0100

    fix js

--- src/main/webapp/js/alimentazione.js
+++ src/main/webapp/js/alimentazione.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
 
     setRecipeRandom("");
-        console.log("e0");
     $("#cerca").click(function(){
-        console.log("e1");
         setRecipeRandom($("#inputIngredients").val());
     });
     
@@ -11,10 +9,8 @@ $(document).ready(function(){
 });
 //ingredients=ciniewnsd,cdnsicns
 function setRecipeRandom(ingredients){
-    console.log("e2");
-    console.log("i: "+ingredients);
-    /*
-    $.getJSON("/RecipeServlet?ingrdients="+ingredients, function(json){
+
+    $.getJSON("/getRecipes?ingrdients="+ingredients, function(json){
 
         updateRecipeList(json);
 				
@@ -27,29 +23,28 @@ function setRecipeRandom(ingredients){
     })
             .always(function() {
                 console.log( "complete" );
-    });*/
+    });
 
 }
 
 function updateRecipeList(json){
-   
-    for (var i = 0; i < json.ricette.length && i<3; i++){
+	for (var i = 0; i < json.recipes.length && i<3; i++){
 		
 		
-        $("#list").append( "<div class=\"row\">"+
-                "<div class=\"col-md-7\">"+
-                "<a href=\""+json.ricette[i].id+".jpg\">"+
-                "<img class=\"img-responsive\" src=\"http://placehold.it/700x300\">"+
-                "</a>"+
-                "</div>"+
-                "<div class=\"col-md-5\">"+
-                "<h3 id=\"nomeRicetta\">"+json.ricette[i].name+"</h3>"+
-                "<h4>Totale Calorie: <span id=\tot\"></span></h4>"+
-                "<p>Descrizione Ricetta:  <span id=\"descRicetta\"></span></p>"+
-                "<a class=\"btn btn-primary\" href=\"buonappetito.html\">Accetta <span class=\"glyphicon glyphicon-chevron-right\"></span></a>"+
-                "</div>"+
-                "</div>" );
-    }
+		$("#ricette").append( "<div class=\"row\">"+
+			"<div class=\"col-md-7\">"+
+			"<a href=\""+json.recipes[i].id+".jpg\">"+
+			"<img class=\"img-responsive\" src=\"http://placehold.it/700x300\">"+
+			"</a>"+
+			"</div>"+
+			"<div class=\"col-md-5\">"+
+			"<h3 id=\"nomeRicetta\">"+json.recipes[i].name+"</h3>"+
+			"<p>Descrizione Ricetta:  <span id=\"descRicetta\">"+json.recipes[i].instructions+"</span></p>"+
+			"<a class=\"btn btn-primary\" href=\"buonappetito.html\">Accetta <span class=\"glyphicon glyphicon-chevron-right\"></span></a>"+
+			"</div>"+
+			"</div>" +
+			"<br><br><br>" );
+	}
 
 }
 
