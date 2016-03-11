0aefac44d54c4fe01cde439f6c4f68428b77232f
EnricoMagnago <en.magnago@gmail.com>
2016-03-11 12:53:56 +0100

    forgot add

---
+++ src/main/java/com/Dietack/Control/AddHistoryServlet.java
@@ -0,0 +1,55 @@
+package com.Dietack.Control;
+
+import com.Dietack.Model.Bean.Recipe;
+import com.Dietack.Model.Bean.User;
+import com.Dietack.Model.RecipeModel;
+import com.Dietack.Model.UserModel;
+
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * Created by enrico on 11/03/16.
+ */
+
+public class AddHistoryServlet extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int idRecipe = Integer.parseInt(request.getParameter("idRicetta"));
+		int idUser = 1;//Integer.parseInt(request.getParameter("idUtente"));
+
+		Recipe recipe = null;
+		try {
+			recipe = RecipeModel.getRecipeById(idRecipe);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+
+		User user = null;
+		try {
+			user = UserModel.getUserById(idUser);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+		if(user == null) {
+			response.getWriter().write("null user");
+		}else if(recipe == null) {
+			response.getWriter().write("null recipe");
+		}else {
+			user.addEatEvent(recipe);
+			response.getWriter().write("done");
+		}
+	}
+}
